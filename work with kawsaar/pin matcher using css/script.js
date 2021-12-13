b8e1cc2cc2788c4ab6243544526bb2b416a9e47d
alaminWeb <alaminmogal@gmail.com>
2021-12-13 16:58:30 +0600

    update

--- work with kawsaar/pin matcher using css/script.js
+++ work with kawsaar/pin matcher using css/script.js
@@ -1,30 +1,33 @@
-// const generatePinBtn = document.getElementsById("generateBtn");
-// generatePinBtn.addEventListener("click", function(){
-//     const randomPin = Math.floor((Math.random()*9000)+1000);
-//     document.getElementsByClassName("pin_field").value = randomPin;
-// })
 const generatePinBtn = document.getElementById("generatePinBtn");
-generatePinBtn.addEventListener("click",()=>{
-    const randomPin = Math.floor((Math.random()*9000)+1000);
+generatePinBtn.addEventListener("click", () => {
+    const randomPin = Math.floor((Math.random() * 9000) + 1000);
     document.getElementById("generatedPinField").value = randomPin;
 })
-// const man = document.getElementById("numBtn");
-// man.addEventListener("click", ()=>{
-//     let val = document.getElementById("numBtn").innerText;
-//     document.getElementById("pinInputField").value = val;
-// })
-
-// let allNumBtns = document.getElementsByClassName('numbtn');
-// for(i=0; i < allNumBtns.length ; i++){
-//     console.log(allNumBtns[i]);
-// }
-
-function numPad(value){
-document.getElementById("pinInputField").value = pinInputField.value + value;
+function numPad(value) {
+    document.getElementById("pinInputField").value = pinInputField.value + value;
 }
-function clearDis(){
+function clearDis() {
     document.getElementById("pinInputField").value = "";
 }
-function backspace(){
-    document.getElementById("pinInputField").value = document.getElementById("pinInputField").value.slice(0,document.getElementById("pinInputField").value.length -1);
+function backspace() {
+    document.getElementById("pinInputField").value = document.getElementById("pinInputField").value.slice(0, document.getElementById("pinInputField").value.length - 1);
+}
+function pinSubmit() {
+    if (generatedPinField.value == pinInputField.value) {
+        alert("Pin Matched");
+    }
+    else {
+        let totalTry = document.getElementById("totalTry").innerText;
+        let tryLeft =parseInt(totalTry) - 1;
+        document.getElementById("totalTry").innerText = tryLeft;
+        if (tryLeft <1) {
+            document.getElementById("tryMsg").innerText = "No more try left";
+            document.getElementById("tryMsg").style.color="red"
+           let submitBtn =  document.getElementById("submitBtn");
+           submitBtn.disabled = true;
+           submitBtn.classList.add("disBtn")
+            
+        }
+    }
+
 }
