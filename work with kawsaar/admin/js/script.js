2439b57b2a4cdc478d33ee1981a96599bbcb463f
alaminWeb <alaminmogal@gmail.com>
2021-12-16 03:44:22 +0600

    update
    menu and toggle button

--- work with kawsaar/admin/js/script.js
+++ work with kawsaar/admin/js/script.js
@@ -1,3 +1,23 @@
-function moreMenu(){
-    document.getElementById("popup_menus").style.display = "block";
+// function moreMenu() {
+//     document.getElementById("popup_menus").style.display = "block";
+// }
+let arrow = document.querySelectorAll(".arrow");
+for (let i = 0; i < arrow.length; i++) {
+    arrow[i].addEventListener("click", (e) => {
+        let arrowParent = e.target.parentElement.parentElement;
+        console.log(arrowParent);
+        arrowParent.classList.toggle("show");
+    });
+
 }
+
+let sidebar = document.querySelector(".left_sidebar");
+console.log(sidebar);
+let  toggleBtn  = document.querySelector(".toggle_bar");
+console.log(toggleBtn);
+toggleBtn.addEventListener("click", ()=>{
+    console.log("clik");
+    sidebar.classList.toggle("close");
+    // let r =document.querySelector(".right_side_body");
+    
+})
