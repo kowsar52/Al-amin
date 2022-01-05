fe985b171297a0284afc35f80fef321625c18c94
alaminWeb <alaminmogal@gmail.com>
2022-01-05 22:30:03 +0600

    update

--- work with kawsaar/bluberry admin template/js/script.js
+++ work with kawsaar/bluberry admin template/js/script.js
@@ -1,5 +1,10 @@
 $('.dropdown_menu').on('click', function(){
+    console.log($(this).next());
     $(this).next().slideToggle('fast')
+    $(this).find('.arrow').toggleClass('rotate')
+})
+$('.profile').on('click', function(){
+    $('.user_menu').fadeToggle('fast')
 })
 
 
