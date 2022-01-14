793b2d7ed75002de0c89eecf3cb7d005007dfcb4
alaminWeb <alaminmogal@gmail.com>
2022-01-14 11:10:21 +0600

    update

--- work with kawsaar/edumark template/js/script.js
+++ work with kawsaar/edumark template/js/script.js
@@ -1,6 +1,13 @@
-$('.page_dropdown').hover(function(){
-    $('.dr_page').slideToggle('fast')
-})
-$('.blog_dropdown').hover(function(){
-    $('.dr_blog').slideToggle('fast')
+// $('.page_dropdown').hover(function(){
+//     $('.dr_page').slideToggle('fast')
+// })
+// $('.blog_dropdown').hover(function(){
+//     $('.dr_blog').slideToggle('fast')
+// })
+
+
+$('.bar_icon').on('click', function(){
+    $('.navbar').toggleClass('nav_show')
+    // $('.navbar').fadeToggle('slow')
+    // (this).toggleClass('.nav_show');
 })
