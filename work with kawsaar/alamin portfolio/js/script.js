08c202eb8028a401b597463254a92256d75c3364
alaminWeb <alaminmogal@gmail.com>
2022-01-24 16:14:31 +0600

    update

--- work with kawsaar/alamin portfolio/js/script.js
+++ work with kawsaar/alamin portfolio/js/script.js
@@ -50,12 +50,17 @@ tabs.forEach(tab=>{
 })
 
 
+ 
 
 // responsive menu bar 
 $('.bar_icon').on('click', function(){
    $('.navbar').toggleClass('nav_show')
 
 })
+$('.navbar .nav_item').on("click", function(){
+   $('.navbar').removeClass('nav_show')
+
+ });
 
 
 
@@ -86,10 +91,10 @@ $('.slider').slick({
      {
        breakpoint: 1024,
        settings: {
-         slidesToShow: 3,
-         slidesToScroll: 3,
-         infinite: true,
-         dots: true
+         slidesToShow: 1,
+         slidesToScroll: 1,
+         // infinite: true,
+         // dots: true
        }
      },
      {
